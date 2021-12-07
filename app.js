36ab211b340e1af7f4b0b65d71ad44cac181430e
Mootez Saad <mootez.saad@medtech.tn>
2021-12-07 13:17:01 +0100

    export app.js

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret: 'keith'}));
+app.use(session({ secret: 'keith' }));
 
 app.use('/', indexRouter);
 app.use('/chatrooms', chatRoomsRouter);
@@ -39,10 +39,10 @@ app.use('/messages', messagesRouter);
 app.use('/r', rRouter(io));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
@@ -50,48 +50,56 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err,
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {},
+  });
 });
 
 var peopleOnline = 0;
-io.on('connection', function(socket) {
-    peopleOnline++;
-    io.emit('user connected', peopleOnline);
+io.on('connection', function (socket) {
+  peopleOnline++;
+  io.emit('user connected', peopleOnline);
 
-    socket.on('chat message', function(msgInfo) {
-        // Send that message to everyone.
-        io.emit('chat message', msgInfo);
-        
-        // Insert that message to database
-        db.insertMessage(msgInfo.room_name, msgInfo.username, msgInfo.msg, msgInfo.unix_time, function(err) {
-            if (err) {
-                console.log('Error while inserting message into db: ' + err);
-            }
-        });
-    });
+  socket.on('chat message', function (msgInfo) {
+    // Send that message to everyone.
+    io.emit('chat message', msgInfo);
 
-    socket.on('disconnect', function() {
-        peopleOnline--;
-        io.emit('user disconnected', peopleOnline);
-    });
+    // Insert that message to database
+    db.insertMessage(
+      msgInfo.room_name,
+      msgInfo.username,
+      msgInfo.msg,
+      msgInfo.unix_time,
+      function (err) {
+        if (err) {
+          console.log('Error while inserting message into db: ' + err);
+        }
+      }
+    );
+  });
+
+  socket.on('disconnect', function () {
+    peopleOnline--;
+    io.emit('user disconnected', peopleOnline);
+  });
 });
 
-http.listen(PORT, function() {
-    console.log('Listening on port ' + PORT);
+http.listen(PORT, function () {
+  console.log('Listening on port ' + PORT);
 });
+
+module.exports = app;
