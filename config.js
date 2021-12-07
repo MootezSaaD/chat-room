a86f696c52105998318fb0f341a0afe26483cdc3
Mootez Saad <mootez.saad@medtech.tn>
2021-12-07 12:55:55 +0100

    add database string as env var

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+
 module.exports = {
-    // Our database filename
-    // anything://user:password@host:port/database
-    chat_db_filename: 'chat.db',
-    pg_server_url: 'postgres://postgres:postgres@localhost:5432/chatter',
-    pg_local_url: 'postgres://keithy@localhost:5432/chatter'
+  // Our database filename
+  // anything://user:password@host:port/database
+  chat_db_filename: 'chat.db',
+  pg_server_url: process.env.DATABASE_URL,
+  pg_local_url: 'postgres://keithy@localhost:5432/chatter',
 };
